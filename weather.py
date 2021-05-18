26fc46eae116d33496f6510d952edf6c9e6f8f17
lalmanza <38576115+lalmanza@users.noreply.github.com>
2021-05-18 13:46:51 -0400

    attempt1

--- weather.py
+++ weather.py
@@ -1,10 +1,9 @@
-temperature = 75;
-forecast = "rain"
-if temperature >80:
-    print ("It's too hot!")
-    print ("Stay inside!")
-elif temperature < 60:
-    print ("It's too cold!")
-    print ("Stay inside!")
-else:
-        print ("enjoy the outdoors!")
+import requests
+
+api_key = "eb6969714d99e1858cd7b03f0e2c34ec"
+city = "Orlando"
+url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key
+
+request = requests.get(url)
+json = requests.json()
+print(json)
---
+++ weather1.py
@@ -0,0 +1,10 @@
+temperature = 75;
+forecast = "rain"
+if temperature >80:
+    print ("It's too hot!")
+    print ("Stay inside!")
+elif temperature < 60:
+    print ("It's too cold!")
+    print ("Stay inside!")
+else:
+        print ("enjoy the outdoors!")
