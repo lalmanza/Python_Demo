c3f03f51c6753f753223c0c11d0fe9a7460d35a1
lalmanza <38576115+lalmanza@users.noreply.github.com>
2021-05-18 14:13:03 -0400

    3 added

---
+++ addition.py
@@ -0,0 +1,11 @@
+# Creating our addition function
+def addition (a, b):
+    return a + b
+
+# Main Program
+num1 = float(input('Enter your 1st number:\n'))
+num2 = float(input('Enter your 2cd number:\n'))
+
+#Calling function
+result = addition(num1, num2)
+print('The result is', result)
---
+++ greetings.py
@@ -0,0 +1,6 @@
+def greeting(name):
+    print('Hello', name)
+
+#Main Program
+input_name = input('Enter your name:\n')
+greeting(input_name)
